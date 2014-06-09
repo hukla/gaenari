b4b9a0f5c957fdb4c424c1d3f962f60afd9d8ed4
sujin <sujinlee611@gmail.com>
2014-06-09 22:24:39 +0900

    commit

--- WebContent/board/scripts/fboard.js
+++ WebContent/board/scripts/fboard.js
@@ -69,7 +69,6 @@ function checkValid() {
 	return true;
 }
 function popupOpen(brdno) {
-	alert('popUp!');
     var popUrl = "image/board/" + brdno + ".jpg"; //팝업창에 출력될 페이지 URL
     var popOption = "width=370, height=360, resizable=no, scrollbars=no, status=no;"; //팝업창 옵션(optoin)
     window.open(popUrl, "", popOption);
